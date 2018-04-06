02126f9e9ab90b0459fe7d66a1526eaa665ad977
PingWIN7 <m.kiss.csaba@gmail.com>
2018-04-06 18:22:34 +0100

    Extracted QrCodeUrlBuilder class

--- project-fob/Controllers/HostController.cs
+++ project-fob/Controllers/HostController.cs
@@ -38,31 +38,9 @@ namespace project_fob.Controllers
         {
             Meeting meet = db.Meeting.Single(x => x.MeetingId.Equals(meetingIdString));
 
-            return CreateUrlForTesting(meetingIdString, Request.GetDisplayUrl(), meet.RoomPassword);
+            return QrCodeUrlBuilder.BuildUrl(meetingIdString, Request.GetDisplayUrl(), meet.RoomPassword);
         }
-
-        public static string CreateUrlForTesting(string meetingIdString, string currentUrl, string roomPassword)
-        {
-            string[] split = currentUrl.Split('/');
-
-            // This variable is really confusing so I'll explain.
-            // Baseurl doesn't work because the url can be a long string which I could not use to correctly find the required parameters to enter to the meeting.
-            // In case we are running the project on a link like : test.co.uk/project/seesharp/fob/
-            // the url request will return something like this: test.co.uk/project/seesharp/fob/index/hostpage. Because of that I have to modify the
-            // URL so it grabs the correct url (hopefully).
-            // The url editing also needed because of the controll handling it'd generate a wrong url for the user which would render the QRCode useless.
-
-            StringBuilder currentUrlLocationBase = new StringBuilder();
-
-            for (int i = 0; i < split.Length - 2; i++)
-            {
-                currentUrlLocationBase.Append(split[i]+"/");
-            }
-            
-            //The string (url) we generate.
-            return currentUrlLocationBase.ToString() + "Home/meetingPageUser?meetingId=" + meetingIdString + "&password=" + roomPassword;
-        }
-
+        
         public ActionResult Finish(string message)
         {
             HttpContext.Session.TryGetValue("meetingid", out var meetingIdValue);
