a42ab0c2e4a15ea40c193ce182ef10d188922ac8
PingWIN7 <m.kiss.csaba@gmail.com>
2018-04-06 16:01:35 +0100

    Added test for QRCode url generating and changed the method.

--- project-fob/Controllers/HostController.cs
+++ project-fob/Controllers/HostController.cs
@@ -36,15 +36,13 @@ namespace project_fob.Controllers
 
         public string CreateUrl(string meetingIdString)
         {
-            // This is really confusing so I'll explain.
-            // Baseurl doesn't work because the url can be a long string which I could not use to correctly find the required parameters to enter to the meeting.
-            // In case we are running the project on a link like : test.co.uk/project/seesharp/fob/
-            // the url request will return something like this: test.co.uk/project/seesharp/fob/index/hostpage. Because of that I have to modify the
-            // URL so it grabs the correct url (hopefully).
-            // The url editing also needed because of the controll handling it'd generate a wrong url for the user which would render the QRCode useless.
-            // Not to mention by default the host url and the QRCode url will be slightly different because of the redirecting it requires a bit editing.
+            Meeting meet = db.Meeting.Single(x => x.MeetingId.Equals(meetingIdString));
 
-            string currentUrl = Request.GetDisplayUrl();
+            return CreateUrlForTesting(meetingIdString, Request.GetDisplayUrl(), meet.RoomPassword);
+        }
+
+        public static string CreateUrlForTesting(string meetingIdString, string currentUrl, string roomPassword)
+        {
             string[] split = currentUrl.Split('/');
 
             // This variable is really confusing so I'll explain.
@@ -58,13 +56,11 @@ namespace project_fob.Controllers
 
             for (int i = 0; i < split.Length - 2; i++)
             {
-                currentUrlLocationBase.Append(split[i]);
+                currentUrlLocationBase.Append(split[i]+"/");
             }
-            Meeting meet = db.Meeting.Single(x => x.MeetingId.Equals(meetingIdString));
             
             //The string (url) we generate.
-            @ViewBag.url = currentUrlLocationBase.ToString() + "/Home/meetingPageUser?meetingId=" + meetingIdString + "&password=" + meet.RoomPassword;
-            return sb.ToString() + "/Home/meetingPageUser?meetingId=" + meetingIdString + "&password=" + meet.RoomPassword;
+            return currentUrlLocationBase.ToString() + "Home/meetingPageUser?meetingId=" + meetingIdString + "&password=" + roomPassword;
         }
 
         public ActionResult Finish(string message)
---
+++ unit-tests/HostControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using project_fob.Controllers;
+using Xunit;
+using FluentAssertions;
+
+namespace unit_tests
+{
+    public class HostControllerTests
+    {
+        [Fact]
+        public void CreateUrlTests()
+        {
+            string meetingId = "meeting-id";
+            const string baseUrl = "http://localhost:53612/Host/QrCode";
+            const string roomPassword = "123456";
+
+            var meetingUrl = HostController.CreateUrlForTesting(meetingId, baseUrl, roomPassword);
+            
+            meetingUrl.Should().Be("http://localhost:53612/Home/meetingPageUser?meetingId=meeting-id&password=123456");
+        }
+    }
+}
