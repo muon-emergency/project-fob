4b185a2504146dee69a8d2bc68d3e5242aac3d2c
PingWIN7 <m.kiss.csaba@gmail.com>
2018-04-06 16:16:49 +0100

    Added a test for long url

--- unit-tests/HostControllerTests.cs
+++ unit-tests/HostControllerTests.cs
@@ -15,8 +15,21 @@ namespace unit_tests
             const string roomPassword = "123456";
 
             var meetingUrl = HostController.CreateUrlForTesting(meetingId, baseUrl, roomPassword);
-            
+
             meetingUrl.Should().Be("http://localhost:53612/Home/meetingPageUser?meetingId=meeting-id&password=123456");
         }
+
+        [Fact]
+        public void CreateUrlFromLongUrlTests()
+        {
+            string meetingId = "meeting-id";
+            const string baseUrl = "http://localhost:53612/myprojects/personal/csharp/Host/QrCode";
+            const string roomPassword = "123456";
+
+            var meetingUrl = HostController.CreateUrlForTesting(meetingId, baseUrl, roomPassword);
+
+            meetingUrl.Should().Be("http://localhost:53612/myprojects/personal/csharp/Home/meetingPageUser?meetingId=meeting-id&password=123456");
+        }
+
     }
 }
