07c069e1667d80cb629286427032292d28d86126
PingWIN7 <m.kiss.csaba@gmail.com>
2018-05-24 16:29:13 +0100

    Added tests

---
+++ unit-tests/IDGeneratorsTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using project_fob.Controllers;
+using FluentAssertions;
+
+namespace unit_tests
+{
+    public class IDGeneratorsTest
+    {
+        [Fact]
+        public void GenerateGuidId()
+        {
+            string guid = IDGenerators.GenerateId();
+            Assert.NotEmpty(guid);
+        }
+
+        [Fact]
+        public void GenerateMeetingId()
+        {
+            string meetingId = IDGenerators.GenerateMeetingId();
+            Assert.NotEmpty(meetingId);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(11)]
+        [InlineData(12)]
+        [InlineData(7)]
+        [InlineData(5)]
+        [InlineData(9)]
+        [InlineData(2)]
+        public void GenerateUnambiguousMeetingIdByLength(int length)
+        {
+            string meetingId = IDGenerators.GenerateUnambiguousMeetingIdByLength(length);
+            meetingId.Length.Should().Be(length);
+        }
+
+        
+
+
+    }
+}
---
+++ unit-tests/MeetingHandlerTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using project_fob.Controllers;
+using project_fob.Data;
+using project_fob.Models;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace unit_tests
+{
+    public class DatabaseFixture : IDisposable
+    {
+        public ApplicationDbContext DbContext { get; private set; }
+
+        public int value { get; }
+
+
+        public DatabaseFixture()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "testdatabase").Options;
+            DbContext = new ApplicationDbContext(options);
+        }
+
+        public void Dispose()
+        {
+            DbContext.Dispose();
+        }
+    }
+
+    public class MeetingHandlerTest : IClassFixture<DatabaseFixture>
+    {
+
+        DatabaseFixture dbFixture;
+
+        public MeetingHandlerTest(DatabaseFixture dbFixture)
+        {
+            this.dbFixture = dbFixture;
+        }
+
+        [Theory]
+        [InlineData("asd", "hey", "holly")]
+        [InlineData("proly", "", "aa")]
+        [InlineData("peep", "krub", "puddle")]
+        public void CreateMeeting(string meetingId, string attendeePassword, string hostPassword)
+        {
+            Meeting meeting = MeetingHandler.CreateMeetingWithUniqueId(meetingId, attendeePassword, hostPassword, dbFixture.DbContext);
+            bool succeeded = false;
+
+
+            if (!meeting.RoomPassword.Equals(attendeePassword))
+            {
+                attendeePassword.Should().Be(meeting.RoomPassword);
+            }
+            if (!meeting.HostPassword.Equals(hostPassword))
+            {
+                hostPassword.Should().Be(meeting.HostPassword);
+            }
+            if (!meeting.MeetingId.Equals(meetingId))
+            {
+                meetingId.Should().Be(meeting.MeetingId.ToString());
+            }
+            succeeded = true;
+            succeeded.Should().Be(true);
+        }
+
+        [Theory]
+        [InlineData("test", "hey", "hello")]
+        [InlineData("test55", "ban", "kick")]
+        [InlineData("xorand", "xand", "or")]
+        public void CheckDuplicateId(string meetingId, string attendeePassword, string hostPassword)
+        {
+            Meeting meeting = MeetingHandler.CreateMeetingWithUniqueId(meetingId, attendeePassword, hostPassword, dbFixture.DbContext);
+            dbFixture.DbContext.SaveChanges();
+
+            Meeting meeting2 = MeetingHandler.CreateMeetingWithUniqueId(meetingId, attendeePassword, hostPassword, dbFixture.DbContext);
+            meeting2.MeetingId.Equals(meeting.MeetingId).Should().Be(false);
+        }
+
+
+    }
+}
